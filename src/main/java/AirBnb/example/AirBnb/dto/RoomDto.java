4e3af0c906a4e7597d985d8b3e24cbb60e678ab0
Sahosrabdi <sahosrabdidas.2016@gmail.com>
2026-03-04 22:00:48 +0530

    Hotel controller and interface for hotel

---
+++ src/main/java/AirBnb/example/AirBnb/dto/RoomDto.java
@@ -0,0 +1,15 @@
+package AirBnb.example.AirBnb.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class RoomDto {
+    private Long id;
+    private String type;
+    private BigDecimal basePrice;
+    private String[] photos;
+    private String[] amenities;
+    private Integer totalCount;
+    private Integer capacity;
+}
